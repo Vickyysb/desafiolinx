ead272273c9d08ccf60a96bf7006d4705375c2c1
Vitoria <vitoriasb95@gmail.com>
2020-12-05 12:11:06 -0300

    Página responsiva ok.

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php include "./src/html/parts/header.html" ?>
+    <link href="./src/scss/css/style.css" rel="stylesheet"/> 
+
+	<meta charset="utf-8"/>
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+	<title>Linx Challenge</title>
+
+</head>
+    <body>
+        <?php include "./src/html/content.html" ?>
+
+        <?php include "./src/html/parts/footer.html" ?>
+    </body>
+</html>
